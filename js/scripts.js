a5515bf7762b7438d7dd805b37c7d33ad1f14b07
joshottey <joshua.ottey@gmail.com>
2018-07-01 08:10:25 -0400

    First working commit of scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,19 @@
 document.onreadystatechange = function () {
   if (document.readyState === "interactive") {
-    fetch('../assets/data/wisdom.json')
+
+    let quotes;
+
+    fetch('https://raw.githubusercontent.com/joshottey/TeaWisdom/master/assets/data/wisdom.json')
       .then(response => response.json())
       .then(data => {
-        console.log(data)
+        quotes = data.yogi.wisdom;
       });
+
+    function newQuote() {
+      let choice = Math.floor(Math.random() * (quotes.length));
+      document.querySelector("#quote").innerHTML = quotes[choice];
+    }
+
+    document.querySelector('#new-quote').addEventListener("click", newQuote);
   }
 }
